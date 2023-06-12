97f51be7e28671a4491af717961beaa9377583a7
anthonychao <qoosuperman@gmail.com>
2023-06-12 22:46:56 +0800

    feat: url encode

--- outline_generator.rb
+++ outline_generator.rb
@@ -2,12 +2,14 @@
 
 # usage: ./outline_generator.rb source/_posts/2021-08-25-Ssh_tunnel.md
 
+require 'cgi'
+
 file_path = ARGV.first
 
 File.readlines("./#{file_path}").each do |line|
  next unless line =~ /^## /
 
  title = line.match(/^## (.*)/)[1]
- link_title = title.split.map(&:downcase).join('-')
+ link_title = title.split.map(&:downcase).map { |str| CGI.escape(str) }.join('-')
  puts "- [#{title}](##{link_title})"
 end
